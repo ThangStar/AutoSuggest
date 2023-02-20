68ad666d23999fec701ecc489f9164f57c253c32
NongVanThang <nguyenquocthinhit@gmail.com>
2023-02-20 17:37:25 +0700

    main

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
 
-export default function App() {
+const data = [
+  "apple",
+  "samsung",
+  "sony",
+  "nokia",
+  "huawei",
+  "htc"
+]
+var dataTarget = [
+
+]
+
+const App = () => {
+  const [value, setValue] = useState('')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <View style={{
+        justifyContent: 'center',
+        paddingVertical: 100,
+        paddingHorizontal: 12
+      }}>
+        <TextInput
+          value={value}
+          style={{
+            borderWidth: 1
+          }}
+          onChangeText={(text) => {
+            setValue(text)
+            dataTarget = []
+            for (var i = 0; i < data.length; i++) {
+              if (data[i].includes(text)) {
+                dataTarget.push(data[i])
+              }
+            }
+          }} />
+        {
+          dataTarget.map((item) => {
+            return (
+              <ItemSuggest suggest = {item} />
+            )
+          })
+        }
+        <View style={{
+          marginTop: 40
+        }}>
+
+        </View>
+      </View>
+
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const ItemSuggest = (props) => {
+  let { suggest } = props
+  return (
+    <View style={{
+      paddingHorizontal: 12,
+      paddingVertical: 8,
+      borderColor: '#F62F63',
+      borderWidth: 1,
+      backgroundColor: '#f62f6431',
+      marginVertical: 1,
+      borderRadius: 12
+    }}>
+      <Text>{suggest}</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
